ed239805a2977d8a0492fe3ea2bdf3cc8dc51c57
Rain-Gayming <raingayming@gmail.com>
2024-07-18 22:56:40 +0100

    Start

---
+++ src/test/java/com/example/MommyPluginTest.java
@@ -0,0 +1,13 @@
+package com.rain.mommy;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class MommmyPluginTest
+{
+	public static void main(String[] args) throws Exception
+	{
+		ExternalPluginManager.loadBuiltin(MommyPlugin.class);
+		RuneLite.main(args);
+	}
+}
